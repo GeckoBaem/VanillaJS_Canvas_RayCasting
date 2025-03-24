d2065f8a4b1854e4d9d8cb123795a3b056e68331
GeckoBaem <geckobaem@gmail.com>
2025-03-24 20:19:18 +0900

    refactor: update canvas dimensions and improve map rendering logic

--- main.js
+++ main.js
@@ -4,36 +4,52 @@ const context = screen.getContext('2d');
 const fps = 120;
 const cycleDelay = Math.floor(1000 / fps);
 
-const width = 1920;
-const height = 1080;
+const width = window.innerWidth;
+const height = width * 9 / 16;
 
-const mapSize = 16;
-const mapScale = 50;
+const mapSize = 32;
+const mapScale = 30;
 const mapRange = mapSize * mapScale;
 const mapSpeed = (mapScale / 2) / 10;
 
 var map = [
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1,
-    1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
 ];
+var showMap = false;
 
 var playerX = mapScale + 20;
 var playerY = mapScale + 20;
-var playerAngle = Math.PI / 3;
+var playerAngle = Math.PI * 3;
 var playerMoveX = 0;
 var playerMoveY = 0;
 var playerMoveAngle = 0;
@@ -42,8 +58,9 @@ document.onkeydown = function (event) {
     switch (event.keyCode) {
         case 40: playerMoveX = -1; playerMoveY = -1; break;
         case 38: playerMoveX = 1; playerMoveY = 1; break;
-        case 37: playerMoveAngle = -1; break;
-        case 39: playerMoveAngle = 1; break;
+        case 37: playerMoveAngle = 1; break;
+        case 39: playerMoveAngle = -1; break;
+        case 77: showMap = true; break;
     }
 }
 
@@ -53,6 +70,7 @@ document.onkeyup = function (event) {
         case 38: playerMoveX = 0; playerMoveY = 0; break;
         case 37:
         case 39: playerMoveAngle = 0; break;
+        case 77: showMap = false; break
     }
 }
 
@@ -61,7 +79,11 @@ const fov = Math.PI / 3;
 const halfFov = fov / 2;
 const stepAngle = fov / width;
 
+// 메인 반복문
 function canvasLoop() {
+    screen.setAttribute('width', width + 'px');
+    screen.setAttribute('height', height + 'px');
+
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
@@ -70,6 +92,8 @@ function canvasLoop() {
     var mapTargetX = Math.floor(playerY / mapScale) * mapSize + Math.floor((playerX + playerOffsetX * playerMoveX) / mapScale);
     var mapTargetY = Math.floor((playerY + playerOffsetY * playerMoveY) / mapScale) * mapSize + Math.floor(playerX / mapScale);
 
+
+    //벽 감지 및 이동 제한
     if (playerMoveX && map[mapTargetX] == 0) playerX += playerOffsetX * playerMoveX;
     if (playerMoveY && map[mapTargetY] == 0) playerY += playerOffsetY * playerMoveY;
     if (playerMoveAngle) playerAngle += 0.03 * playerMoveAngle;
@@ -77,47 +101,14 @@ function canvasLoop() {
     var mapOffsetX = Math.floor(width / 2 - mapRange / 2);
     var mapOffsetY = Math.floor(height / 2 - mapRange / 2);
 
-    for (let row = 0; row < mapSize; row++) {
-        for (let col = 0; col < mapSize; col++) {
-            let cell = map[row * mapSize + col];
-            if (cell == 1) {
-                context.fillStyle = '#555';
-                context.fillRect(
-                    col * mapScale + mapOffsetX,
-                    row * mapScale + mapOffsetY,
-                    mapScale,
-                    mapScale
-                );
-            } else {
-                context.fillStyle = '#aaa';
-                context.fillRect(
-                    col * mapScale + mapOffsetX,
-                    row * mapScale + mapOffsetY,
-                    mapScale,
-                    mapScale
-                );
-            }
-        }
-    }
-
     var playerMapX = playerX + mapOffsetX;
     var playerMapY = playerY + mapOffsetY;
-
-    context.fillStyle = 'Red';
-    context.beginPath();
-    context.arc(playerMapX, playerMapY, 10, 0, doublePI);
-    context.fill();
-    context.strokeStyle = 'Red';
-    context.lineWidth = 10;
-    context.beginPath();
-    context.moveTo(playerMapX, playerMapY);
-    context.lineTo(playerMapX + Math.cos(playerAngle) * 30, playerMapY + Math.sin(playerAngle) * 30);
-    context.stroke();
 
     var currentAngle = playerAngle + halfFov;
     var rayStartX = Math.floor(playerX / mapScale) * mapScale;
     var rayStartY = Math.floor(playerY / mapScale) * mapScale;
 
+    //광선 계산
     for (var ray = 0; ray < width; ray++) {
         var currentCos = Math.cos(currentAngle); currentCos = currentCos ? currentCos : 0.000001;
         var currentSin = Math.sin(currentAngle); currentSin = currentSin ? currentSin : 0.000001;
@@ -138,10 +129,9 @@ function canvasLoop() {
             rayEndX += rayDirectionX * mapScale;
         }
 
-        var tempX = rayEndX; tempY = rayEndY;
-
+        
         var rayEndY, rayEndX, rayDirectionY, horizontalDepth;
-
+        
         var rayEndY, rayEndX, rayDirectionY, horizontalDepth;
         if (currentSin > 0) { rayEndY = rayStartY + mapScale; rayDirectionY = 1 }
         else { rayEndY = rayStartY; rayDirectionY = -1 }
@@ -156,22 +146,75 @@ function canvasLoop() {
             if (map[targetSquare] != 0) break;
             rayEndY += rayDirectionY * mapScale;
         }
-        var endX = verticalDepth < horizontalDepth ? tempX : rayEndX;
-        var endY = verticalDepth < horizontalDepth ? tempY : rayEndY;
 
-        context.strokeStyle = 'Yellow';
-        context.lineWidht = 1;
-        context.beginPath();
-        context.moveTo(playerMapX, playerMapY);
-        context.lineTo(endX + mapOffsetX, endY + mapOffsetY);
-        context.stroke();
+        // var tempX = rayEndX; tempY = rayEndY;
+
+        // var endX = verticalDepth < horizontalDepth ? tempX : rayEndX;
+        // var endY = verticalDepth < horizontalDepth ? tempY : rayEndY;
+        
+        // context.strokeStyle = 'Yellow';
+        // context.lineWidth = 0.1;
+        // context.beginPath();
+        // context.moveTo(playerMapX, playerMapY);
+        // context.lineTo(endX + mapOffsetX, endY + mapOffsetY);
+        // context.stroke();
+
+        //1인칭 화면 출력
+        var depth = verticalDepth < horizontalDepth ? verticalDepth : horizontalDepth;
+        depth *= Math.cos(playerAngle - currentAngle);
+        var wallHeight = Math.min(mapScale * 2000 / (depth + 0.0001), height);
+        context.fillStyle = verticalDepth < horizontalDepth ? '#aaa' : '#555';
+
+        context.fillRect(
+            ray,
+            height / 2 - wallHeight / 2,
+            1,
+            wallHeight
+        );
 
         currentAngle -= stepAngle;
-}
+    }
+
+    //'M' 클릭 시 지도 출력
+    if (showMap) {
+        for (let row = 0; row < mapSize; row++) {
+            for (let col = 0; col < mapSize; col++) {
+                let cell = map[row * mapSize + col];
+                if (cell == 1) {
+                    context.fillStyle = '#555';
+                    context.fillRect(
+                        col * mapScale + mapOffsetX,
+                        row * mapScale + mapOffsetY,
+                        mapScale,
+                        mapScale
+                    );
+                } else {
+                    context.fillStyle = '#aaa';
+                    context.fillRect(
+                        col * mapScale + mapOffsetX,
+                        row * mapScale + mapOffsetY,
+                        mapScale,
+                        mapScale
+                    );
+                }
+            }
+        }
+
+        context.fillStyle = 'Red';
+        context.beginPath();
+        context.arc(playerMapX, playerMapY, 10, 0, doublePI);
+        context.fill();
+        context.strokeStyle = 'Red';
+        context.lineWidth = 10;
+        context.beginPath();
+        context.moveTo(playerMapX, playerMapY);
+        context.lineTo(playerMapX + Math.cos(playerAngle) * 30, playerMapY + Math.sin(playerAngle) * 30);
+        context.stroke();
+    }
 
 
 
-setTimeout(canvasLoop, cycleDelay);
+    setTimeout(canvasLoop, cycleDelay);
 }
 
 window.onload = (event) => { canvasLoop() };
